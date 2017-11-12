2a496b1b6968500392c11b1abb86d34029e8e1a5
NLoewen05 <nic_loewen@yahoo.ca>
2017-11-12 22:42:00 +0000

    Part 2 new controller, finished

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :customers do
+    get 'alphabetized', on: :collection
+    get 'missing_email', on: :collection
+  end
+  root 'customers#index'
 end
